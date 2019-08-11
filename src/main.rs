eba382311f79158f912597cdb6aeaf308bd33ccc
rubio <rubio@RETTERBLOND.retterblond.net>
2019-08-11 13:07:56 +0200

    setq bug solved, added arithmetic op

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate regex;
 use regex::Regex;
 use std::io::BufRead;
+use std::mem;
 // use std::io;
 #[derive(Debug, Clone)]
 enum Expr {
@@ -71,7 +72,7 @@ fn car(expr: Box<Expr>) -> Box<Expr> {
     match *expr {
         Expr::Cons(car, _cdr) => car,
         _ => {
-            println!("Error. {:?} is not a list", print_car(expr));
+            println!("Error. {:?} is not a list", print_car(&expr));
             Box::new(Expr::Nil)
         }
     }
@@ -81,7 +82,7 @@ fn cdr(expr: Box<Expr>) -> Box<Expr> {
     match *expr {
         Expr::Cons(_car, cdr) => cdr,
         _ => {
-            println!("Error. {:?} is not a list", print_car(expr));
+            println!("Error. {:?} is not a list", print_car(&expr));
             Box::new(Expr::Nil)
         }
     }
@@ -115,26 +116,26 @@ fn assoc(x: Box<Expr>, env: Box<Expr>) -> Box<Expr> {
                 }
             }
             _ => {
-                println!("Error. {:?} is not a list", print_car(first));
+                println!("Error. {:?} is not a list", print_car(&first));
                 Box::new(Expr::Nil)
             }
         },
         Expr::Nil => {
-            println!("Error. {:?} is undefined", print_car(x));
+            println!("Error. {:?} is undefined", print_car(&x));
             Box::new(Expr::Nil)
         }
         _ => {
-            println!("Error. {:?} is not a list", print_car(env));
+            println!("Error. {:?} is not a list", print_car(&env));
             Box::new(Expr::Nil)
         }
     }
 }
 
-fn evcond(expr: Box<Expr>, env: Box<Expr>) -> Box<Expr> {
-    let cond = eval(caar(expr.clone()), env.clone()).0;
+fn evcond(expr: Box<Expr>, env: &Box<Expr>, global_env: &mut Box<Expr>) -> Box<Expr> {
+    let cond = eval(caar(expr.clone()), env, global_env);
     match *cond {
-        Expr::Nil => evcond(cdr(expr), env),
-        _ => eval(cadar(expr), env).0,
+        Expr::Nil => evcond(cdr(expr), env, global_env),
+        _ => eval(cadar(expr), env, global_env),
     }
 }
 
@@ -155,12 +156,28 @@ fn plus(a: Box<Expr>, b: Box<Expr>) -> Box<Expr> {
         Expr::Number(value_a) => match *b {
             Expr::Number(value_b) => Box::new(Expr::Number(value_a + value_b)),
             _ => {
-                println!("Error. {:?} is not a number", print_car(b));
+                println!("Error. {:?} is not a number", print_car(&b));
                 Box::new(Expr::Nil)
             }
         },
         _ => {
-            println!("Error. {:?} is not a number", print_car(a));
+            println!("Error. {:?} is not a number", print_car(&a));
+            Box::new(Expr::Nil)
+        }
+    }
+}
+
+fn arithmetic_op(a: Box<Expr>, b: Box<Expr>, f: &Fn(i32, i32) -> i32) -> Box<Expr> {
+    match *a {
+        Expr::Number(value_a) => match *b {
+            Expr::Number(value_b) => Box::new(Expr::Number(f(value_a, value_b))),
+            _ => {
+                println!("Error. {:?} is not a number", print_car(&b));
+                Box::new(Expr::Nil)
+            }
+        },
+        _ => {
+            println!("Error. {:?} is not a number", print_car(&a));
             Box::new(Expr::Nil)
         }
     }
@@ -184,32 +201,32 @@ fn pair(x: Box<Expr>, y: Box<Expr>) -> Box<Expr> {
             Expr::Cons(head_y, tail_y) => cons(list(head_x, head_y), pair(tail_x, tail_y)),
             Expr::Nil => Box::new(Expr::Nil),
             _ => {
-                println!("Error. {:?} is not a list", print_car(y));
+                println!("Error. {:?} is not a list", print_car(&y));
                 Box::new(Expr::Nil)
             }
         },
         Expr::Nil => Box::new(Expr::Nil),
         _ => {
-            println!("Error. {:?} is not a list", print_car(x));
+            println!("Error. {:?} is not a list", print_car(&x));
             Box::new(Expr::Nil)
         }
     }
 }
 
-fn evlis(arguments: Box<Expr>, env: Box<Expr>) -> Box<Expr> {
+fn evlis(arguments: Box<Expr>, env: &Box<Expr>, global_env: &mut Box<Expr>) -> Box<Expr> {
     match *arguments {
-        Expr::Cons(head, tail) => cons(eval(head, env.clone()).0, evlis(tail, env)),
+        Expr::Cons(head, tail) => cons(eval(head, env, global_env), evlis(tail, env, global_env)),
         Expr::Nil => Box::new(Expr::Nil),
         _ => {
-            println!("Error. {:?} is not a list", print_car(arguments));
+            println!("Error. {:?} is not a list", print_car(&arguments));
             Box::new(Expr::Nil)
         }
     }
 }
 
 fn main() {
-    let mut result: (Box<Expr>, Box<Expr>);
-    let mut env: Box<Expr> = parse("((QUOTE QUOTE) (ATOM ATOM) (EQ EQ) (CAR CAR) (CDR CDR) (CONS CONS) (LIST LIST) (COND COND) (PLUS PLUS) (LAMBDA LAMBDA) (MACRO MACRO) (SETQ SETQ) (NIL ()))".to_string());
+    let mut result: Box<Expr>;
+    let env: &mut Box<Expr> = &mut parse("((QUOTE QUOTE) (ATOM ATOM) (EQ EQ) (CAR CAR) (CDR CDR) (CONS CONS) (LIST LIST) (COND COND) (PLUS PLUS) (PROD PROD) (DIFF DIFF) (QUOT) (QUOT) (LAMBDA LAMBDA) (MACRO MACRO) (SETQ SETQ) (NIL ()))".to_string());
     loop {
         //       let mut input = String::new();
         //        io::stdin().lock..expect("error reading");
@@ -217,160 +234,142 @@ fn main() {
         for line in input.lock().lines() {
             // here line is a String without the trailing newline
             let parsed = parse(line.unwrap().to_uppercase());
-            result = eval(parsed, env);
-            env = result.1.clone();
-            println!("{}", print_car(result.0));
-            println!("env: {}", print_car(result.1));
+            result = eval(parsed, &env.clone(), env);
+            println!("{}", print_car(&result));
+            // println!("env: {}", print_car(env));
         }
     }
 }
 
-fn eval(expr: Box<Expr>, env: Box<Expr>) -> (Box<Expr>, Box<Expr>) {
+fn eval(expr: Box<Expr>, env: &Box<Expr>, global_env: &mut Box<Expr>) -> Box<Expr> {
     //    let expr2 = Box::new((*expr).clone());
     //    let env2 = Box::new((*env).clone());
     match *expr {
-        Expr::T => (Box::new(Expr::T), env),
-        Expr::Nil => (Box::new(Expr::Nil), env),
-        Expr::Number(num) => (Box::new(Expr::Number(num)), env),
-        Expr::Symbol(symbol) => {
-            //    let init_letter = &symbol[0..1];
-            //    let rest_word = &symbol[1..(symbol.len())];
-            //    if init_letter != "\'" {
-            (assoc(Box::new(Expr::Symbol(symbol)), env.clone()), env)
-            //     } else {
-            //         (
-            //             eval(
-            //                 list(
-            //                     Box::new(Expr::Symbol("QUOTE".to_string())),
-            //                     Box::new(Expr::Symbol(rest_word.to_string())),
-            //                 ),
-            //                 env.clone(),
-            //             )
-            //             .0,
-            //             env,
-            //         )
-            //     }
-        }
+        Expr::T => Box::new(Expr::T),
+        Expr::Nil => Box::new(Expr::Nil),
+        Expr::Number(num) => Box::new(Expr::Number(num)),
+        Expr::Symbol(symbol) => assoc(Box::new(Expr::Symbol(symbol)), (*env).clone()),
 
         Expr::Cons(car_elem, cdr_elem) => match *(car_elem.clone()) {
             Expr::Symbol(symbol) => {
                 //      println!("your value: {:?}", (symbol).clone());
                 if symbol == "QUOTE" {
-                    (car(cdr_elem), env)
+                    car(cdr_elem)
                 } else if symbol == "ATOM" {
-                    (atom(eval(car(cdr_elem), env.clone()).0), env)
-                //    } else if symbol == "NIL" {
-                //        (Box::new(Expr::Nil), env)
+                    atom(eval(car(cdr_elem), env, global_env))
                 } else if symbol == "EQ" {
-                    (
-                        eq(
-                            eval(car(cdr_elem.clone()), env.clone()).0,
-                            eval(cadr(cdr_elem), env.clone()).0,
-                        ),
-                        env,
+                    eq(
+                        eval(car(cdr_elem.clone()), env, global_env),
+                        eval(cadr(cdr_elem), env, global_env),
                     )
                 } else if symbol == "CAR" {
-                    (car(eval(car(cdr_elem), env.clone()).0), env)
+                    car(eval(car(cdr_elem), env, global_env))
                 } else if symbol == "CDR" {
-                    (cdr(eval(car(cdr_elem), env.clone()).0), env)
+                    cdr(eval(car(cdr_elem), env, global_env))
                 } else if symbol == "CONS" {
-                    (
-                        cons(
-                            eval(car(cdr_elem.clone()), env.clone()).0,
-                            eval(cadr(cdr_elem), env.clone()).0,
-                        ),
-                        env,
+                    cons(
+                        eval(car(cdr_elem.clone()), env, global_env),
+                        eval(cadr(cdr_elem), env, global_env),
                     )
                 } else if symbol == "LIST" {
-                    (evlis(cdr_elem, env.clone()), env)
+                    evlis(cdr_elem, env, global_env)
                 } else if symbol == "COND" {
-                    (evcond(cdr_elem, env.clone()), env)
+                    evcond(cdr_elem, env, global_env)
                 } else if symbol == "PLUS" {
-                    (
-                        plus(
-                            eval(car(cdr_elem.clone()), env.clone()).0,
-                            eval(cadr(cdr_elem), env.clone()).0,
-                        ),
-                        env,
+                    arithmetic_op(
+                        eval(car(cdr_elem.clone()), env, global_env),
+                        eval(cadr(cdr_elem), env, global_env),
+                        &|a, b| a + b
+                    )
+                } else if symbol == "PROD" {
+                    arithmetic_op(
+                        eval(car(cdr_elem.clone()), env, global_env),
+                        eval(cadr(cdr_elem), env, global_env),
+                        &|a, b| a * b
+                    )
+                } else if symbol == "DIFF" {
+                    arithmetic_op(
+                        eval(car(cdr_elem.clone()), env, global_env),
+                        eval(cadr(cdr_elem), env, global_env),
+                        &|a, b| a - b
+                    )
+                } else if symbol == "QUOT" {
+                    arithmetic_op(
+                        eval(car(cdr_elem.clone()), env, global_env),
+                        eval(cadr(cdr_elem), env, global_env),
+                        &|a, b| a / b
                     )
                 } else if symbol == "SETQ" {
-                    let value = eval(cadr(cdr_elem.clone()), env.clone()).0;
+                    let value = eval(cadr(cdr_elem.clone()), env, global_env);
                     let name = car(cdr_elem);
-                    println!("setq value {:?}", print_car(value.clone()));
-                    (value.clone(), cons(list(name, value), env))
+                    // println!("setq value {:?}", print_car(&value));
+                    // global_env = &cons(list(name, value.clone()), (*global_env).clone());
+                    mem::swap(
+                        global_env,
+                        &mut cons(list(name, value.clone()), (*global_env).clone()),
+                    );
+                    value
                 } else if symbol == "LAMBDA" || symbol == "MACRO" {
-                    (cons(car_elem, cdr_elem), env)
+                    cons(car_elem, cdr_elem)
                 } else if symbol == "EVAL" {
-                    let newContext = eval(car(cdr_elem), env.clone());
-                    let resp = eval(newContext.0, env);
-                    println!("eval resp {:?}", print_car(resp.0.clone()));
-                    println!("eval env {:?}", print_car(resp.1.clone()));
+                    let new_context = eval(car(cdr_elem), env, global_env);
+                    let resp = eval(new_context, env, global_env);
+                    //  println!("eval resp {:?}", print_car(&resp));
+                    //  println!("eval env {:?}", print_car(&resp));
                     resp
                 } else {
-                    (
-                        eval(
-                            cons(
-                                eval(Box::new(Expr::Symbol(symbol)), env.clone()).0,
-                                cdr_elem,
-                            ),
-                            env.clone(),
-                        )
-                        .0,
+                    eval(
+                        cons(
+                            eval(Box::new(Expr::Symbol(symbol)), env, global_env),
+                            cdr_elem,
+                        ),
                         env,
+                        global_env,
                     )
                 }
             }
             Expr::Cons(car1, cdr1) => match *car1 {
                 Expr::Symbol(symbol) => {
                     if symbol == "LABEL" {
-                        (
-                            eval(
-                                cons(cadr(cdr1.clone()), cdr_elem),
-                                cons(list(car(cdr1), car_elem), env.clone()),
-                            )
-                            .0,
-                            env,
+                        eval(
+                            cons(cadr(cdr1.clone()), cdr_elem),
+                            &cons(list(car(cdr1), car_elem), (*env).clone()),
+                            global_env,
                         )
                     } else if symbol == "LAMBDA" {
-                        (
-                            eval(
-                                cadr(cdr1.clone()),
-                                append(pair(car(cdr1), evlis(cdr_elem, env.clone())), env.clone()),
-                            )
-                            .0,
-                            env,
+                        eval(
+                            cadr(cdr1.clone()),
+                            &append(
+                                pair(car(cdr1), evlis(cdr_elem, env, global_env)),
+                                (*env).clone(),
+                            ),
+                            global_env,
                         )
                     } else if symbol == "MACRO" {
-                        (
-                            eval(
-                                cadr(cdr1.clone()),
-                                append(pair(car(cdr1), cdr_elem), env.clone()),
-                            )
-                            .0,
-                            env,
+                        eval(
+                            cadr(cdr1.clone()),
+                            &append(pair(car(cdr1), cdr_elem), (*env).clone()),
+                            global_env,
                         )
                     } else {
-                        (
-                            eval(
-                                cons(
-                                    cons(assoc(Box::new(Expr::Symbol(symbol)), env.clone()), cdr1),
-                                    cdr_elem,
-                                ),
-                                env.clone(),
-                            )
-                            .0,
+                        eval(
+                            cons(
+                                cons(assoc(Box::new(Expr::Symbol(symbol)), env.clone()), cdr1),
+                                cdr_elem,
+                            ),
                             env,
+                            global_env,
                         )
                     }
                 }
                 _ => {
-                    println!("Error. {:?} is not a symbol", print_car(car1));
-                    (Box::new(Expr::Nil), env)
+                    println!("Error. {:?} is not a symbol", print_car(&car1));
+                    Box::new(Expr::Nil)
                 }
             },
             _ => {
-                println!("Error. {:?} is not a symbol", print_car(car_elem));
-                (Box::new(Expr::Nil), env)
+                println!("Error. {:?} is not a symbol", print_car(&car_elem));
+                Box::new(Expr::Nil)
             }
         },
     }
@@ -473,9 +472,9 @@ fn parse(expr: String) -> Box<Expr> {
     }
 }
 
-fn print_car(expr: Box<Expr>) -> String {
-    match *expr {
-        Expr::Symbol(symbol) => symbol,
+fn print_car(expr: &Expr) -> String {
+    match expr {
+        Expr::Symbol(symbol) => symbol.to_string(),
         Expr::Number(number) => number.to_string(),
         Expr::T => "T".to_string(),
         Expr::Nil => "NIL".to_string(),
@@ -483,9 +482,9 @@ fn print_car(expr: Box<Expr>) -> String {
     }
 }
 
-fn print_cdr(expr: Box<Expr>) -> String {
-    match *expr {
-        Expr::Symbol(symbol) => symbol,
+fn print_cdr(expr: &Expr) -> String {
+    match expr {
+        Expr::Symbol(symbol) => symbol.to_string(),
         Expr::Number(number) => number.to_string(),
         Expr::T => "T".to_string(),
         Expr::Nil => ")".to_string(),
